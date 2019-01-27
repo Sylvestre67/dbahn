5e949dcdd59133b3478f6f958aab8bdde0b83762
Sylvestre Gug <sylvestre.gug@gmail.com>
2019-01-27 12:32:20 -0500

    registered Header, main and Footer to App

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
 
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { withStyles, MuiThemeProvider } from '@material-ui/core/styles';
 
 import theme from './theme/theme';
 
+import apolloClient from './services/apollo';
+
+import Header from './layout/Header';
+import Footer from './layout/Footer';
+
 const styles = theme => ({
-	root: {},
+	main: {
+		flex: '1 0 auto',
+	},
 });
 
 class App extends Component {
 	render() {
+		const { classes } = this.props;
 		return (
 			<MuiThemeProvider theme={theme}>
-				<div className="App">
-					<CssBaseline />
-				</div>
+				<CssBaseline />
+				<Header />
+				<main role="main" className={classes.main} />
+				<Footer />
 			</MuiThemeProvider>
 		);
 	}
