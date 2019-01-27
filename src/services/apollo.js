4b7cef3a185ecaf2c4ed09290ed9d79bc25afd4f
Sylvestre Gug <sylvestre.gug@gmail.com>
2019-01-27 11:51:24 -0500

    added apollo client to available services

---
+++ src/services/apollo.js
@@ -0,0 +1,25 @@
+import { ApolloLink, concat } from 'apollo-link';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+
+const httpLink = new HttpLink({
+	uri: `${process.env.REACT_APP_1BAHN_HOST}/graphql`,
+});
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+	operation.setContext({
+		headers: {
+			Authorization: `Bearer ${process.env.REACT_APP_1BAHN_BEARER}`,
+		},
+	});
+
+	return forward(operation);
+});
+
+const apolloClient = new ApolloClient({
+	link: concat(authMiddleware, httpLink),
+	cache: new InMemoryCache(),
+});
+
+export default apolloClient;
