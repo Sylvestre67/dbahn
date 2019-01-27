a2761ab8ce63289f8afce329c0fd7d0e1cb444c2
Sylvestre Gug <sylvestre.gug@gmail.com>
2019-01-27 18:41:28 -0500

    registered Stations provider to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import gql from 'graphql-tag';
-
 import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
 import { withStyles, MuiThemeProvider } from '@material-ui/core/styles';
 
 import theme from './theme/theme';
@@ -17,9 +16,17 @@ import FourOhFour from './routes/FourOhFour';
 import Search from './routes/Search';
 import Station from './routes/Station';
 
+import Stations from './context/Stations';
+
 const styles = theme => ({
 	main: {
 		flex: '1 0 auto',
+		display: 'flex',
+		justifyContent: 'center',
+		padding: `0 ${theme.spacing.unit * 3}px`,
+	},
+	container: {
+		maxWidth: '1400px',
 	},
 });
 
@@ -30,18 +37,29 @@ class App extends Component {
 			<MuiThemeProvider theme={theme}>
 				<CssBaseline />
 				<Header />
+
 				<main role="main" className={classes.main}>
-					<Router>
-						<Switch>
-							<Route exact path="/" component={Search} />
-							<Route
-								exact
-								path="/:station_id/"
-								component={Station}
-							/>
-							<Route component={FourOhFour} />
-						</Switch>
-					</Router>
+					<Grid className={classes.container} container spacing={8}>
+						<Grid item xs={12}>
+							<Router>
+								<Stations>
+									<Switch>
+										<Route
+											exact
+											path="/"
+											component={Search}
+										/>
+										<Route
+											exact
+											path="/:station_id/"
+											component={Station}
+										/>
+										<Route component={FourOhFour} />
+									</Switch>
+								</Stations>
+							</Router>
+						</Grid>
+					</Grid>
 				</main>
 				<Footer />
 			</MuiThemeProvider>
