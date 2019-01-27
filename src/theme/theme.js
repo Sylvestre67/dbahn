e77c72bb4d30036b7b7e78365617f0bd49fce6e6
Sylvestre Gug <sylvestre.gug@gmail.com>
2019-01-27 11:09:18 -0500

    added theme, favicon and font

---
+++ src/theme/theme.js
@@ -0,0 +1,39 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+	palette: {
+		primary: {
+			light: '#659fd1',
+			main: '#3F87C6',
+			dark: '#2c5e8a',
+		},
+		secondary: {
+			light: '#ffcf33',
+			main: '#ffc400',
+			dark: '#b28900',
+		},
+		error: {
+			light: '#ff8982',
+			main: '#ff5555',
+			dark: '#c5162c',
+		},
+		success: {
+			light: '#54d6a7',
+			main: '#00a478',
+			dark: '#00744c',
+		},
+	},
+	typography: {
+		fontFamily: 'Muli, Helvetica, Arial, sans-serif',
+		useNextVariants: true,
+	},
+	overrides: {
+		MuiGrid: {
+			container: {
+				maxWidth: '1400px',
+			},
+		},
+	},
+});
+
+export default theme;
