000e4c6e22089f24a1f2e0592ed0d4e8f5f0574f
Sylvestre Gug <sylvestre.gug@gmail.com>
2019-01-28 11:35:54 -0500

    Fixed blinking on SearchResults virtualized list

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -2,31 +2,33 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { withStyles } from '@material-ui/core';
-import List from '@material-ui/core/List';
+
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
 
-import { AutoSizer, List as VirtualizedList } from 'react-virtualized';
+import { AutoSizer, List } from 'react-virtualized';
 
 import { StationsContext } from '../context/Stations';
 
-const overscanRowCount = 10;
 const listRowHeight = 61;
 
 const styles = theme => ({
 	listContainer: {
 		height: '100%',
 	},
-	list: {},
+	list: {
+		'&:focus': {
+			outline: 0, // I know you should not do it.
+		},
+	},
 });
 
 class SearchResults extends React.Component {
 	constructor(props) {
 		super(props);
-
 		this._rowRenderer = this._rowRenderer.bind(this);
 	}
 
@@ -36,10 +38,11 @@ class SearchResults extends React.Component {
 
 		return (
 			<div className={classes.listContainer}>
-				{/* <Typography>
+				<Typography>
 					Stations found: {JSON.stringify(data.length, 4, 4)}
 				</Typography>
-				<Typography>
+
+				{/* }<Typography>
 					Loading: {JSON.stringify(loading, 4, 4)}
 				</Typography>
 				<Typography>
@@ -48,80 +51,59 @@ class SearchResults extends React.Component {
 
 				<AutoSizer>
 					{({ width, height }) => (
-						<List>
-							<VirtualizedList
-								ref="List"
-								className={classes.list}
-								height={height - theme.spacing.unit * 3}
-								overscanRowCount={overscanRowCount}
-								noRowsRenderer={this._noRowsRenderer}
-								rowCount={data.length}
-								rowHeight={listRowHeight}
-								rowRenderer={this._rowRenderer}
-								width={width}
-							/>
-						</List>
+						<List
+							className={classes.list}
+							ref="List"
+							width={width}
+							height={height - theme.spacing.unit * 3}
+							rowHeight={listRowHeight}
+							overscanRowCount={5}
+							rowCount={data.length}
+							noRowsRenderer={this._noRowsRenderer}
+							rowRenderer={this._rowRenderer}
+						/>
 					)}
 				</AutoSizer>
 			</div>
 		);
 	}
 
-	_noRowsRenderer() {}
+	_noRowsRenderer() {
+		return <Typography>No stations found.</Typography>;
+	}
 
 	_rowRenderer({ index, isScrolling, key, style }) {
 		const { classes } = this.props;
 		const { data } = this.context;
-		const station = data[index];
+		let station = data[index];
+
+		// Sanitizing the station object.
+		if (Object.is(station.picture, null)) {
+			station = Object.assign(station, { picture: { url: '' } }); // add a default picture url here
+		}
 
 		return (
-			<React.Fragment key={`${index}`}>
-				{isScrolling ? (
-					<ListItem alignItems="flex-start">
-						<ListItemAvatar>
-							<Avatar
-								alt={station.name}
-								src={station.picture.url}
-							/>
-						</ListItemAvatar>
-						<ListItemText
-							primary={station.name}
-							secondary={
-								<React.Fragment>
-									<Typography
-										component="span"
-										className={classes.inline}
-										color="textPrimary">
-										{station.primaryEvaId}
-									</Typography>
-								</React.Fragment>
-							}
-						/>
-					</ListItem>
-				) : (
-					<ListItem alignItems="flex-start" key={`${index}`}>
-						<ListItemAvatar>
-							<Avatar
-								alt={station.name}
-								src={station.picture.url}
-							/>
-						</ListItemAvatar>
-						<ListItemText
-							primary={station.name}
-							secondary={
-								<React.Fragment>
-									<Typography
-										component="span"
-										className={classes.inline}
-										color="textPrimary">
-										{station.primaryEvaId}
-									</Typography>
-								</React.Fragment>
-							}
-						/>
-					</ListItem>
-				)}
-			</React.Fragment>
+			<ListItem
+				alignItems="flex-start"
+				key={station.primaryEvaId}
+				style={style}>
+				<ListItemAvatar>
+					<Avatar alt={station.name} src={station.picture.url} />
+				</ListItemAvatar>
+				<ListItemText
+					primary={station.name}
+					secondary={
+						<React.Fragment>
+							<Typography
+								component="span"
+								color="textPrimary"
+								variant="caption">
+								{station.primaryEvaId}
+							</Typography>
+						</React.Fragment>
+					}
+				/>
+			</ListItem>
 		);
 	}
 }
